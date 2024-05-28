f93eaa4decefc46fd5135de061071c7e402ca14e
nagesh-merva <officialnagesh.merva@gmail.com>
2024-05-28 19:26:12 +0530

    fixing COR issue

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from email.message import EmailMessage
 
 app = Flask(__name__)
+CORS(app)  
 
 user_email = os.environ.get('USER_EMAIL', 'default_user_email')
 user_password = os.environ.get('USER_PASSWORD', 'default_user_password')
@@ -19,23 +20,24 @@ def contact():
     subject = request.form.get('subject')
     message = request.form.get('message')
 
-    app.logger.info(f"Received form data:\nName: {name}\nEmail: {email}\nsubject: {subject}\nmessage: {message}")
+    app.logger.info(f"Received form data:\nName: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}")
 
     try:
         msg = EmailMessage()
-        msg.set_content(f"From: {name}\nEmail: {email}\nsubject: {subject}\nmessage: {message}")
+        msg.set_content(f"From: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}")
 
         msg['Subject'] = f"New contact form submission: {subject}"
-        msg['From'] = email
+        msg['From'] = user_email 
         msg['To'] = receiving_email_address
 
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
-            server.login(user_email,user_password)
+            server.login(user_email, user_password)
             server.send_message(msg)
 
-        return jsonify({'name': name, 'email': email, 'subject': subject, 'message': message})
+        return jsonify({'message': 'Email sent successfully', 'status': 'success'}), 200
 
     except Exception as e:
-        print(f"Error sending email: {e}")
-        return jsonify({'message': 'Error sending email'}), 500
+        app.logger.error(f"Error sending email: {e}")
+        return jsonify({'message': 'Error sending email', 'error': str(e), 'status': 'error'}), 500
+
