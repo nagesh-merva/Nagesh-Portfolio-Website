bdb152823db1438b6316a5cfa288f1f1b8a48d7d
nagesh-merva <officialnagesh.merva@gmail.com>
2024-05-28 19:44:25 +0530

    modified

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,10 @@ from flask_cors import CORS
 from email.message import EmailMessage
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 
-user_email = os.environ.get('USER_EMAIL', 'default_user_email')
-user_password = os.environ.get('USER_PASSWORD', 'default_user_password')
-receiving_email = os.environ.get('RECEIVING_EMAIL')
+user_email = 'portfolionagesh.1957@gmail.com '
+receiving_email = 'portfolionagesh.1957@gmail.com'
+user_password = 'qlqy eoda vftd mtkx'
 
 @app.route('/contact', methods=['POST'])
 def contact():
@@ -20,24 +19,23 @@ def contact():
     subject = request.form.get('subject')
     message = request.form.get('message')
 
-    app.logger.info(f"Received form data:\nName: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}")
+    app.logger.info(f"Received form data:\nName: {name}\nEmail: {email}\nsubject: {subject}\nmessage: {message}")
 
     try:
         msg = EmailMessage()
-        msg.set_content(f"From: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}")
+        msg.set_content(f"From: {name}\nEmail: {email}\nsubject: {subject}\nmessage: {message}")
 
         msg['Subject'] = f"New contact form submission: {subject}"
-        msg['From'] = user_email 
+        msg['From'] = email
         msg['To'] = receiving_email_address
 
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
-            server.login(user_email, user_password)
+            server.login(user_email,user_password)
             server.send_message(msg)
 
-        return jsonify({'message': 'Email sent successfully', 'status': 'success'}), 200
+        return jsonify({'name': name, 'email': email, 'subject': subject, 'message': message})
 
     except Exception as e:
-        app.logger.error(f"Error sending email: {e}")
-        return jsonify({'message': 'Error sending email', 'error': str(e), 'status': 'error'}), 500
-
+        print(f"Error sending email: {e}")
+        return jsonify({'message': 'Error sending email'}), 500
