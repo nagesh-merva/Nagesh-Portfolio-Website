1a537d651332dd18bcdb0990333f804b649479c1
nagesh-merva <officialnagesh.merva@gmail.com>
2025-02-17 23:37:17 +0530

    configured

--- NEXTJS/web/next.config.mjs
+++ NEXTJS/web/next.config.mjs
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-
-    // Enable static rendering for dynamic routes
     trailingSlash: false,
-
-    // Add image domains if you're loading images from external sources
     images: {
         domains: ['*'],
     },
-
-    // Additional experimental features that can help with dynamic routes
     experimental: {
         appDir: true,
     },
 }
 
-module.exports = nextConfig
+export default nextConfig
